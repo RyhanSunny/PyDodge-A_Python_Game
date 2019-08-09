e45a1a45eab11762b98b18b1637c67f9fdff69c5
Ryhan Sunny <SUNNYRAYHAN2@GMAIL.COM>
2019-08-09 13:47:57 +0200

    COLLISION ADDED AND INITIALIZED

--- PyDodge.py
+++ PyDodge.py
@@ -20,7 +20,7 @@ player_size = 50
 player_pos = [(WIDTH / 2) - player_size, HEIGHT - (2 * player_size)]
 
 enemy_size = 50
-enemy_position = [random.randint(0, WIDTH - enemy_size), 0] # for random int, use library see IMPORT 3
+enemy_pos = [random.randint(0, WIDTH - enemy_size), 0]  # for random int, use library see IMPORT 3
 
 enemy_speed = 4
 
@@ -34,7 +34,19 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 game_over = False
 
+
 # FUNCTION 1: DETECTING COLLISION
+def detect_collision(player_pos, enemy_pos):
+    p_x = player_pos[0]
+    p_y = player_pos[1]
+    e_x = enemy_pos[0]
+    e_y = enemy_pos[1]
+
+    if (p_x <= e_x < (p_x + player_size)) or (e_x <= p_x < (e_x + enemy_size)):
+        if (p_y <= e_y < (p_y + player_size)) or (e_y <= p_y < (e_y + enemy_size)):
+            return True
+    return False
+
 
 while not game_over:
     for event in pygame.event.get():
@@ -65,17 +77,20 @@ while not game_over:
     pygame.draw.rect(screen, RED, (player_pos[0], player_pos[1], player_size, player_size))
 
     # A2: drawing enemy
-    # pygame.draw.circle(screen, BLUE, enemy_position, 25)
-    pygame.draw.rect(screen, BLUE, (enemy_position[0], enemy_position[1], enemy_size, enemy_size))
+    # pygame.draw.circle(screen, BLUE, enemy_pos, 25)
+    pygame.draw.rect(screen, BLUE, (enemy_pos[0], enemy_pos[1], enemy_size, enemy_size))
 
     # A3: MAKING ENEMY FALL - UPDATING ENEMY POSITION
-    if 0 <= enemy_position[1] < HEIGHT:
-        enemy_position[1] += enemy_speed
+    if 0 <= enemy_pos[1] < HEIGHT:
+        enemy_pos[1] += enemy_speed
         # at this point we have to set a frame rate to our game, see variable CLOCK and comment #FRAME RATE
     else:
-        enemy_position[1] = 0
-        enemy_position[0] = random.randint(0, WIDTH-enemy_size)
+        enemy_pos[1] = 0
+        enemy_pos[0] = random.randint(0, WIDTH - enemy_size)
     # A4: START BUILDING COLLISION FUNCTION, SEE FUNCTION 1 ABOVE
+    # INITIATING FUNCTION 1
+    if detect_collision(player_pos, enemy_pos):
+        game_over = True
 
     # FRAME RATE
     CLOCK.tick(120)
